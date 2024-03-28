4e7559f19c9e22d1595263b9977e6c372c4be41d
Vinod-V3 <vinod@tunerlabs.com>
2024-03-28 14:32:57 +0530

    Routing implementation for details page done

--- projects/elevate-project-library/src/lib/elevate-project-library.module.ts
+++ projects/elevate-project-library/src/lib/elevate-project-library.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { ElevateProjectLibraryComponent } from './elevate-project-library.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
+import { DetailsPageComponent } from './components/details-page/details-page.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 
+const routes: Routes = [
+  { path:'details', component:DetailsPageComponent }
+]
 
 @NgModule({
   declarations: [
     ElevateProjectLibraryComponent,
-    MainPageComponent
+    MainPageComponent,
+    DetailsPageComponent
   ],
   imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   exports: [
-    MainPageComponent
+    RouterModule
   ]
 })
 export class ElevateProjectLibraryModule { }
